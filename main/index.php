e79ddc2e6161bdad1ac30041c0e22a98ecd074af
Tituss22 <158983092+Tituss22@users.noreply.github.com>
2025-01-22 00:13:08 +0700

    SukaBintang01
    Signed-off-by: Tituss22 <158983092+Tituss22@users.noreply.github.com>

---
+++ main/index.php
@@ -0,0 +1,52 @@
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hacked By SukaBintang01</title>
+    <link rel="icon" href="https://telegra.ph/file/a35b090cf9ec01898604c.jpg" type="image/x-icon">
+    <meta name="description" content="Hacked By SukaBintang01">
+    <meta name="keywords" content="Hacked By SukaBintang01">
+    <meta name="rating" content="General">
+    <meta name="revisit-after" content="1 days">
+    <meta name="classification" content="Hacked By SukaBintang01">
+    <meta name="robots schedule" content="auto">
+    <link href="https://fonts.googleapis.com/css2?family=Oxygen" rel="stylesheet">
+</head>
+<body>
+<style>
+html {
+    background-color: black;
+    color: #000;
+}
+h2 {
+    font-family:"Bold 700 Italic",Mali;
+    color:red;
+}
+h3, h4 {
+    font-family:"Oxygen",serif;
+    color:white;
+}
+a {
+    color: white;
+    text-decoration: none;
+}
+::selection {
+    color:white;
+    background:#000;
+}
+</style>
+<table width="100%" height="100%">
+    <td align="center">
+        <img alt="#FuckBnPp" src="https://telegra.ph/file/f8e5b2de33ddaa512ccf7.jpg" width="450px">
+        <h2>Message From SukaBintang01</h2>        <h4><br> //---------------------------Garuda Security---------------------------//<br>
+        <p style="font-size: 1.5em;">
+            Aku tidak akan berhenti melakukan ini kecuali negara tercinta ku sudah bersih dari korupsi / koruptor.
+            Sudahi korupsi mu mari bertobat bersama ku, ingat banyak masyarakat sengsara di negara mu itu bung
+        </p>
+        <br> //---------------------------Garuda Security---------------------------//<br><br>
+      <br>#TransparansiUntukSemua #StopKorupsi #FreedomFighters #HackForGood
+        <br><br>kami hanya ingin anda berhenti korupsi! </br></h4>
+        <audio controls="controls" src="https://windsports.co.nz/Mereka%20yang%20berdasi%20Lukanegara%20JR.mp3"></audio>
+        <h3> <a href="https://t.me/garudasecurityofficial" >- <font color="red">Official team</font> -<br>Brothe7 - WhiteRose - Awan - saint - Crishbit - Cyber jawa timur - SukaBintang01 - ./FqXploit - Tn_wizyakuza 404</h3>
+</body>
+</html>
