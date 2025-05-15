63c2f55cfcaf9985e57b04f9a505a33b203372ec
Tituss22 <158983092+Tituss22@users.noreply.github.com>
2025-05-15 07:06:53 +0700

    Update index.php
    Signed-off-by: Tituss22 <158983092+Tituss22@users.noreply.github.com>

--- main/index.php
+++ main/index.php
@@ -4,7 +4,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hacked By SukaBintang01</title>
     <link rel="icon" href="https://telegra.ph/file/a35b090cf9ec01898604c.jpg" type="image/x-icon">
-    <meta name="google-site-verification" content="JfWWAb1nzB1s4vCrouNtjerCPS-RsZb5TG8JkzzI5G8" />
     <meta name="description" content="Hacked By SukaBintang01">
     <meta name="keywords" content="Hacked By SukaBintang01">
     <meta name="rating" content="General">
@@ -36,18 +35,16 @@ a {
     background:#000;
 }
 </style>
+<script src="script.js"></script>
 <table width="100%" height="100%">
     <td align="center">
         <img alt="#FuckBnPp" src="https://telegra.ph/file/f8e5b2de33ddaa512ccf7.jpg" width="450px">
-        <h2>Message From SukaBintang01</h2>        <h4><br> //---------------------------Garuda Security---------------------------//<br>
-        <p style="font-size: 1.5em;">
-            Aku tidak akan berhenti melakukan ini kecuali negara tercinta ku sudah bersih dari korupsi / koruptor.
-            Sudahi korupsi mu mari bertobat bersama ku, ingat banyak masyarakat sengsara di negara mu itu bung
-        </p>
+        <h2>Hacked By SukaBintang01</h2>        <h4><br> //---------------------------Garuda Security---------------------------//<br>
+            <br>"MBG=makan beracun goblok tolong perhatikan kesehatan makanan untuk siang kau masak malam dan anak punya alergi masing-masing gk semua anak bisa makan yang sama"</br>
         <br> //---------------------------Garuda Security---------------------------//<br><br>
-      <br>#TransparansiUntukSemua #StopKorupsi #FreedomFighters #HackForGood
-        <br><br>kami hanya ingin anda berhenti korupsi! </br></h4>
-        <audio controls="controls" src="https://windsports.co.nz/Mereka%20yang%20berdasi%20Lukanegara%20JR.mp3"></audio>
-        <h3> <a href="https://t.me/garudasecurityofficial" >- <font color="red">Official team</font> -<br>Brothe7 - WhiteRose - Awan - saint - Crishbit - Cyber jawa timur - SukaBintang01 - ./FqXploit - Tn_wizyakuza 404</h3>
+      <br>#IndonesiaGelap #tolakmbg #mbgKepentinganbpolitik
+        <br><br>Tolak MBG !</br></h4>
+        <audio controls="controls" src="https://a.top4top.io/m_3421hq9ht0.mp3"></audio>
+        <h3> <a href="https://t.me/garudasecurityofficial" >- <font color="red">Official team</font> -<br>  brohder7 - WhiteRose - Awan - saint - Crishbit - Cyber jawa timur - SukaBintang01 - ./FqXploit - Tn_wizyakuza 404</h3>
 </body>
-</html>
+</html
